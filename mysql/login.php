d44fbdcfb80c3a31177c00177c3444cabb9c4f79
Devian <forza9882@gmail.com>
2020-10-28 20:05:46 +0200

    Adding bootstrap framework and HTML mark up.

--- mysql/login.php
+++ mysql/login.php
@@ -3,8 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
+        
+        <div class="col-xs-6">
+            <form action="">
 
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control"> 
+
+                </div>
+
+                <div class="form-group">
+                <label for="password">Password</label>
+                    <input type="password" class="form-control"> 
+
+                </div>
+
+            </div>   
+
+        </div>
+
+    </div>
 </body>
 </html>
